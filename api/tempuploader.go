0f6f909cd3021168dd0aebf7d09f618e8be96a72
samuelIkoli <ayibanimiikoli@gmail.com>
2025-03-11 09:51:13 +0100

    chore: renamed for export

--- api/tempuploader.go
+++ api/tempuploader.go
@@ -2,7 +2,7 @@ package api
 
 import "github.com/gin-gonic/gin"
 
-func (s *Server) uploadFiles(ctx *gin.Context) {
+func (s *Server) UploadFiles(ctx *gin.Context) {
 	// TODO: write out the endpoint here:
 	// temporary  logic to receives files from the endpoint:
 }
