b75202d7cee6cb65341878859c63774cc308c30f
Emmanuel Emonueje Ebenezer <eebenezer949@gmail.com>
2025-03-11 13:24:01 +0100

    feat: add method to generate ai response to queries

--- telexcom/telexcom.go
+++ telexcom/telexcom.go
@@ -1,6 +1,19 @@
 package telexcom
 
+import "context"
+
 const (
 	BaseTelexAPIProduction = "https://api.telex.im/api/v1/"
 	BaseTelexAPIStaging    = "https://api.staging.telex.im/api/v1/"
 )
+
+type TelexCom struct {
+	// Have the AI interface,
+	// Have the Repository interface
+}
+
+func (txc *TelexCom) GenerateResponseToQuery(ctx context.Context, query string) error {
+	// Call the AI Service
+	// Call the Repository ser
+	return nil
+}
